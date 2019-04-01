6691bb175b167831a23a7dedcc1e4954bfc688ee
hupeng <hupeng@xiaomi.com>
2019-04-01 17:39:47 +0800

    add hp-demo

---
+++ hp-demo/src/main/java/com/xiaomi/hp/leetcode/Problem5.java
@@ -0,0 +1,19 @@
+package com.xiaomi.hp.leetcode;
+
+public class Problem5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public String longestPalindrome(String s) {
+		
+		if (s == null || s.length() == 0) {
+			return s;
+		}
+		
+        return null;
+    }
+
+}
---
+++ hp-demo/src/main/java/com/xiaomi/hp/thread/Demo.java
@@ -0,0 +1,55 @@
+package com.xiaomi.hp.thread;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
+public class Demo {
+	
+	public static void main(String[] args){
+
+        int count = 3;
+        CountDownLatch latch = new CountDownLatch(count);
+
+        long start = System.currentTimeMillis();
+        for (int i = 0;i<count;i++) {
+            Thread thread = new Thread(new TestThread(latch));
+            thread.start();
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("time: " + (System.currentTimeMillis() - start));
+
+    }
+
+    public static class TestThread implements Runnable {
+
+        private CountDownLatch latch;
+
+        public TestThread(CountDownLatch latch) {
+            this.latch = latch;
+        }
+
+        @Override
+        public void run() {
+            long start = System.currentTimeMillis();
+            System.out.println("start " + Thread.currentThread().getName());
+            Random random = new Random();
+            try {
+                int time = random.nextInt(50000);
+                System.out.println("" + Thread.currentThread().getName() + " sleep time: " + time);
+                Thread.sleep(time);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("end " + Thread.currentThread().getName() + ", cost: " + (System.currentTimeMillis() - start));
+            latch.countDown();
+        }
+    }
+	
+}
